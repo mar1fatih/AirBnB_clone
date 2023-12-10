b13c4297c96b715e6473765d81d54646c4ae1524
ISSAM-SLI <issam.slimani.maroc@gmail.com>
2023-12-10 08:04:43 -0800

    Tasks

--- tests/test.py
+++ tests/test.py
@@ -15,15 +15,8 @@ class Test_Amenity(unittest.TestCase):
         """ Tests the pep8 """
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(['models/amenity.py'])
-        self.assertEqual(result.total_errors, 0, "Check pep8")
-
-    def test_Amenity_dict(self):
-        """Amenity_dict"""
-        self.assertTrue('id' in self.amenity.__dict__)
-        self.assertTrue('created_at' in self.amenity.__dict__)
-        self.assertTrue('updated_at' in self.amenity.__dict__)
-        self.assertTrue('name' in self.amenity
-    def trad()
+        self.assertEqual(result.total_errors, 0, "Check pep
+        """
 
 if __name__ == '__main__':
     unittest.main()
